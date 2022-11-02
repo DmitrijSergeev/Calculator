b7f5e066931254d4f297a3a1216b67c89823a350
Dmitry <dmitrijsergeev@gmail.com>
2022-11-02 10:51:19 +0400

    рефакторинг кода

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
-    let multiPly = document.querySelector('.pow'),
-        devide = document.querySelector('.share'),
-        summ = document.querySelector('.plus'),
-        left = document.querySelector('.minus');
-    let input1 = document.querySelector('.first'),
-        input2 = document.querySelector('.second'),
-        input3 = document.querySelector('input.answer');
+        let multiPly = document.querySelector('.pow'),
+                devide = document.querySelector('.share'),
+                summ = document.querySelector('.plus'),
+                left = document.querySelector('.minus');
+        let input1 = document.querySelector('.first'),
+                input2 = document.querySelector('.second'),
+                input3 = document.querySelector('input.answer'),
+                result;
 
-        function getNumber1 (){
-            return Number(input1.value);
+        function getNumber1() {
+                return Number(input1.value);
         }
-        function getNumber2 (){
-            return Number(input2.value);
+        function getNumber2() {
+                return Number(input2.value);
+        }
+        function makeOperation(operationCode) {
+                if (operationCode === '*') {
+                        result = getNumber1() * getNumber2();
+                } else if (operationCode === '/') {
+                        result = getNumber1() / getNumber2();
+                } else if ((operationCode === '+')) {
+                        result = getNumber1() + getNumber2();
+                } else if ((operationCode === '-')) {
+                        result = getNumber1() - getNumber2();
+                } else {
+                        alert('Operation is unknown');
+                }
+                alert(result);
         }
 
-        function onButtonMultiply (){
-
-                let sum = getNumber1()*getNumber2();
-                
-                alert(sum);
-        };
-        function onButtonDevide (){
-            
-                let sum = getNumber1()/getNumber2();
-
-                alert(sum);
-        };
-        function onButtonSumm (){
-
-                let sum = getNumber1()+getNumber2();
-
-                alert(sum);
-        };
-        function onButtonSubstract (){
-
-                let sum = getNumber1()-getNumber2();
-
-                alert(sum);
-        };
+        function onButtonMultiply() {
+                makeOperation('*');
+        }
+        function onButtonDevide() {
+                makeOperation('/');
+        }
+        function onButtonSumm() {
+                makeOperation('+');
+        }
+        function onButtonSubstract() {
+                makeOperation('-');
+        }
 
         multiPly.addEventListener('click', onButtonMultiply);
         devide.addEventListener('click', onButtonDevide);
